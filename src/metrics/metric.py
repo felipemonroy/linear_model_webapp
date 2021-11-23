6751cdd1e06812bfe1ec2ae0873b1c602594e699
Felipe Monroy <felipe.m02@gmail.com>
2021-11-23 11:55:07 -0500

    Moving init from metrics

--- src/metrics/metric.py
+++ src/metrics/metric.py
@@ -4,18 +4,9 @@ Models performance metrics.
 
 from abc import ABC, abstractmethod
 
-import numpy as np
-from numpy.typing import ArrayLike
-from src.models.model import Model
-
 
 class Metrics(ABC):
     """Abstract representation of a metric"""
-
-    def __init__(self, model: Model, X: ArrayLike, y: ArrayLike):
-        self.y_true = np.array(y)
-        self.X = np.array(X)
-        self.y_pred = model.predict(self.X)
 
     @property
     @abstractmethod
