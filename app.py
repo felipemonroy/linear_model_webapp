adcf0f108008f7db5b732f10e03f09717021829d
Felipe Monroy <felipe.m02@gmail.com>
2021-11-25 14:33:09 -0500

    Removing canvas

--- app.py
+++ app.py
@@ -2,7 +2,10 @@
 App
 """
 
-from app.canvas import Canvas
+from app.pages.page import MultiPage, SideBar
+from app.pages.pages import DiagnosticPage, MetricPage, OverallPage
+from src.metrics.metrics import RegressionMetrics
+from src.models.models import LinearRegressionExpanded
 from src.settings.settings import AppConfig
 
 
@@ -11,8 +14,26 @@ def main():
 
     parameters = AppConfig()
 
-    app = Canvas(parameters)
-    app.build()
+    sidebar = SideBar(parameters.APP_NAME)
+    sidebar.render()
+
+    if sidebar.data:
+        model = LinearRegressionExpanded()
+        model.fit(sidebar.data.X, sidebar.data.y)
+
+        metrics = RegressionMetrics(model, sidebar.data.X, sidebar.data.y)
+
+        page_overall = OverallPage(parameters)
+        page_metric = MetricPage(parameters, metrics)
+        page_diagnostic = DiagnosticPage(parameters)
+
+        page = MultiPage()
+
+        page.add_page(page_overall)
+        page.add_page(page_metric)
+        page.add_page(page_diagnostic)
+
+        page.run()
 
 
 if __name__ == "__main__":
